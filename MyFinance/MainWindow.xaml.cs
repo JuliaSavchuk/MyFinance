0c61496567dd61848ba81c343d7c03957321d91a
JuliaSavchuk <161220472+JuliaSavchuk@users.noreply.github.com>
2025-03-11 20:45:35 +0200

    Update MainWindow.xaml.cs

--- MyFinance/MainWindow.xaml.cs
+++ MyFinance/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using MyFinance.Models;
+using MyFinance.Models;
 using MyFinance.Views;
 using System.Windows;
 
@@ -6,6 +6,8 @@ namespace MyFinance
 {
     public partial class MainWindow : Window
     {
+        private User _currentUser;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,10 +19,12 @@ namespace MyFinance
             TabsPanel.Children.Clear();
             TabsPanel.Children.Add(new AuthorisationView());
             SetButtonsEnabled(false);
+            _currentUser = null;
         }
 
         public void ShowMainView(User user)
         {
+            _currentUser = user;
             TabsPanel.Children.Clear();
             TabsPanel.Children.Add(new MainView(user));
             SetButtonsEnabled(true);
@@ -31,8 +35,22 @@ namespace MyFinance
             ShowAuthorisationView();
         }
 
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+
+                ShowMainView(_currentUser);
+            }
+            else
+            {
+                MessageBox.Show("No user is currently logged in.");
+            }
+        }
+
         private void SetButtonsEnabled(bool isEnabled)
         {
+            RefreshButton.IsEnabled = isEnabled;
             ExitButton.IsEnabled = isEnabled;
         }
     }
